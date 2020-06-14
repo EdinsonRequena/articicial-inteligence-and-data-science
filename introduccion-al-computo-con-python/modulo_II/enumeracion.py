cc08039a8fa475ef034789899dbd4e48d4503cc8
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-14 04:06:16 -0400

    adding enumeracion.py

---
+++ introduccion-al-computo-con-python/modulo_II/enumeracion.py
@@ -0,0 +1,20 @@
+"""
+
+Tema: Algoritmo de enumeracion exahustiva.
+Curso: Pensamiento computacional
+Plataforma: Platzi
+Profesos: Davied Areosti
+
+"""
+
+objetivo = int(input("Escribe un numero entero: "))
+respuesta = 0
+
+while respuesta**2 < objetivo:
+    print(respuesta)
+    respuesta += 1
+
+if respuesta**2 == objetivo:
+    print(f'La raiz cuadrada de {objetivo} es {respuesta}')
+else:
+    print(f'{objetivo} no tiene una raiz cuadrada exacta')
