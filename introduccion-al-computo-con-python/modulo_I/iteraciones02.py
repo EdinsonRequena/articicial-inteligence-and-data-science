72283b1d57ed01d2e9aa10b58c8d1abd213688a8
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-13 20:26:21 -0400

    adding interaciones02.py

---
+++ introduccion-al-computo-con-python/modulo_I/iteraciones02.py
@@ -0,0 +1,22 @@
+"""
+Escriba un programa que simule un relog usando bucles while
+
+-------------------- Algoritmo ------------------------
+1) Crear tres variables de tipo entero que almacenen la hora, los minutos y los segundos e inicalizarlas en 0.
+
+2) Crear un bucle while que recorra la variable hora 24 veces
+
+3) Dentro del bucle que recorre la variable hora crear otro bucle que recorra la variable min 60 veces
+
+4) Dentro del bucle que recorre la variable min crear otro bucle que recorra la variable seg 60 veces
+
+5) Dentro del bucle que recorre la variable seg imprimir por pantalla el valor de todas las variables recorridas
+
+6) Dentro del bucle que recorre la variable seg sumarle uno a la variable seg.
+
+7) Dentro del bucle que recorre la variable min (pero fuera del bucle que recorre la variable seg) 
+sumarle uno a min y resetrar a cero la variable seg.
+
+8) Dentro del bucle que recorre la variable hora (pero fuera del bucle que recorre la variable min) 
+sumarle uno a hora y resetear a cero la variable min.
+"""
