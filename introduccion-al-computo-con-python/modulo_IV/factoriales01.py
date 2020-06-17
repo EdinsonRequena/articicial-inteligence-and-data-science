8ccb599e45295bc55e3e8cf3511817f728569f79
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-17 13:09:53 -0400

    adding factoriales01.py

---
+++ introduccion-al-computo-con-python/modulo_IV/factoriales01.py
@@ -0,0 +1,26 @@
+# Importamos el modulo sys que nos permitira modificar la recursion
+import sys
+
+def factorial(n):
+    """Calcula el factorial de n
+
+        n int > 0
+        returns n!
+    """
+    print(n)
+    if n == 1:
+        return 1
+    
+    return n * factorial(n - 1)
+
+if __name__ == "__main__":
+
+    n = int(input('Escribe un entero: '))
+
+    # Imprimimos el limite de recursion que trae python por defecto con la funcion sys.getrecursionlimit() que trae el modulo sys. 
+    print(sys.getrecursionlimit())
+
+    # Modificamos el limite de recursion a 2000 con la funcion sys.setrecursionlimit() que trae el modulo sys
+    sys.setrecursionlimit(2000)
+
+    print(factorial(n))
