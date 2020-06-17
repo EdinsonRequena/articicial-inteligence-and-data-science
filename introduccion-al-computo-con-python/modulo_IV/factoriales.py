91c538a429090b1631aa89bb38277ec35085aff3
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-17 04:41:11 -0400

    update factoriales.py

--- introduccion-al-computo-con-python/modulo_IV/factoriales.py
+++ introduccion-al-computo-con-python/modulo_IV/factoriales.py
@@ -7,3 +7,19 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+def factorial(n):
+    """Calcula el factorial de n
+
+        n int > 0
+        returns n!
+    """
+    print(n)
+    if n == 1:
+        return 1
+    
+    return n * factorial(n - 1)
+
+a = int(input("Escribe un entero: "))
+
+print(factorial(a))
