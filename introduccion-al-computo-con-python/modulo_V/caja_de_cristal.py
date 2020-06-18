6a838dfeb6c8ac1090db97c535a4d464632cbfbc
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 18:41:27 -0400

    update

--- introduccion-al-computo-con-python/modulo_V/caja_de_cristal.py
+++ introduccion-al-computo-con-python/modulo_V/caja_de_cristal.py
@@ -8,3 +8,29 @@ Alumno: @edinsonrequena.
 
 """
 
+import unittest
+
+def mayor_de_edad(edad):
+    if edad >= 18:
+        return True
+    else:
+        return False
+
+class PruebaDeCristalTest(unittest.TestCase):
+    def test_es_mayor(self):
+        edad = 12
+
+        resultado = mayor_de_edad(edad)
+
+        self.assertEqual(resultado, True)
+    
+
+    def test_es_menor(self):
+        edad = 20
+
+        resultado = mayor_de_edad(edad)
+
+        self.assertEqual(resultado, False)
+
+if __name__ == '__main__':
+    unittest.main()
