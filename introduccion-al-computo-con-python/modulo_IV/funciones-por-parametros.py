42e5e835ce6bd2a3172816414ca6d02cc1c90d4a
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 00:08:32 -0400

    update

--- introduccion-al-computo-con-python/modulo_IV/funciones-por-parametros.py
+++ introduccion-al-computo-con-python/modulo_IV/funciones-por-parametros.py
@@ -7,3 +7,25 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+def multiplicar_por_dos(n):
+    return n * 2
+
+
+def sumar_dos(n):
+    return n + 2
+
+
+def aplicar_operacion(funcion, numeros):
+    resultados = []
+    for numero in numeros:
+        resultado = funcion(numero)
+        resultados.append(resultado)
+    
+    print(resultados)
+
+if __name__ == '__main__':
+    nums = [5, 10, -4, 14/10]
+
+    aplicar_operacion(multiplicar_por_dos, nums)
+    aplicar_operacion(sumar_dos, nums)
