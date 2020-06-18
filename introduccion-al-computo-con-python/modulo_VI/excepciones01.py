a688be7e46513f1d59f815b9907efeec5ac683ee
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 19:59:23 -0400

    update excepciones01.py

---
+++ introduccion-al-computo-con-python/modulo_VI/excepciones01.py
@@ -0,0 +1,10 @@
+"""
+
+Tema: Manejo de excepciones y uso del principio EAFP 
+(easier to ask for forgiveness than permission, es más fácil pedir perdón que permiso, por sus siglas en inglés)..
+Curso: Pensamiento computacional.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+
+"""
