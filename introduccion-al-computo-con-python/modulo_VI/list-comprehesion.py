01f00bc45e8b032d27b2a260ebb0baf5299fe2d3
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 13:08:55 -0400

    update

--- introduccion-al-computo-con-python/modulo_VI/list-comprehesion.py
+++ introduccion-al-computo-con-python/modulo_VI/list-comprehesion.py
@@ -7,3 +7,12 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+lista = list(range(100)) # Creamos una lista con elementos que esten en el rango de 0 a 99.
+print(lista)
+
+double = [i * 2 for i in lista] # Multiplicamos por 2 los elementos de la lista.
+print(double)
+
+pares = [i for i in lista if i % 2 == 0] # Aca iteramos los elementos en lista si o solo si estos son divisibles entre 2, o sea, son pares.
+print(pares)
