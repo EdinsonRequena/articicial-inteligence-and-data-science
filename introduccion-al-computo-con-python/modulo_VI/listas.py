1c879e842e333ecfaa03a79db7135e041ff05ef5
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 11:17:33 -0400

    update

--- introduccion-al-computo-con-python/modulo_VI/listas.py
+++ introduccion-al-computo-con-python/modulo_VI/listas.py
@@ -7,3 +7,12 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+my_list = [1, 'dos', 3, True, 'cuatro', 5]
+print(my_list[0]) # Imprime indice 0
+print(my_list[:2]) # Imprime desde el inidce 0 hasta el 2
+print(my_list[1:3]) # Imprime desde indice 1 hasta el 3
+print(my_list[2:]) # Imprime desde el indice 2 hasta el ultimo
+
+my_list.append(1) # Agrega un elemento al ultimo indice de la lista, los elementos en una lista pueden repetirse.
+print(my_list)
