6668b0264e6e6a4ed72ee8c52de465ecc4686242
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 19:46:15 -0400

    udpate

--- introduccion-al-computo-con-python/modulo_VI/excepciones.py
+++ introduccion-al-computo-con-python/modulo_VI/excepciones.py
@@ -8,3 +8,15 @@ Alumno: @edinsonrequena.
 
 """
 
+def division(lista, divisor):
+    try:
+        return [i / divisor for i in lista]
+    except ZeroDivisionError as error:
+        print(f'The error is: {error}')
+        return lista
+
+if __name__ == '__main__':
+    lista = list(range(10))
+    divisor = int(input('Escribe un numero: '))
+
+    print(division(lista, divisor))
