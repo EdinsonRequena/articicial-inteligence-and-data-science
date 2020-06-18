9cca98b83410eb1c49722291063481ce9226ad9c
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 00:48:43 -0400

    adding range.py

---
+++ introduccion-al-computo-con-python/modulo_VI/range.py
@@ -0,0 +1,9 @@
+"""
+
+Reto: Imprimir todos los numeros pares e impares del 1 al 100
+Curso: Pensamiento computacional.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+
+"""
