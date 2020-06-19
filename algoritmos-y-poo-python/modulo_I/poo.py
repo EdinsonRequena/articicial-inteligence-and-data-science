44966931b7e614d43a9d809d53eabdbb2a81fb81
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-19 03:21:13 -0400

    update

--- algoritmos-y-poo-python/modulo_I/poo.py
+++ algoritmos-y-poo-python/modulo_I/poo.py
@@ -7,3 +7,31 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+# Recordatorio, describir brevemente las lineas de codigo
+class Hotel:
+    
+    def __init__(self, numero_maximo_de_huespedes, lugares_de_estacionamiento): # Utilizamos el método especial __init__ 
+                                                                                # para definir el estado inicial de nuestra instancia.
+        self.numero_maximo_de_huespedes = numero_maximo_de_huespedes
+        self.lugares_de_estacionamiento = lugares_de_estacionamiento
+        self.huespedes = 0
+
+
+    def anadir_huespedes(self, cantidad_de_huespedes):
+        self.huespedes += cantidad_de_huespedes
+        return self.huespedes, cantidad_de_huespedes
+
+    def checkout(self, a):
+        self.huespedes -= a
+
+
+    def ocupacion_total(self):
+        return self.huespedes
+
+
+
+hotel = Hotel(50, 20)
+print(hotel.numero_maximo_de_huespedes, hotel.lugares_de_estacionamiento)
+print(hotel.anadir_huespedes(3))
+print(hotel.checkout(1))
+print(hotel.ocupacion_total())
