ca76dcf4981a92b3d7afabf1a0a4518838d16fe1
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-18 20:08:23 -0400

    update excepciones01.py

--- introduccion-al-computo-con-python/modulo_VI/excepciones01.py
+++ introduccion-al-computo-con-python/modulo_VI/excepciones01.py
@@ -8,3 +8,27 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+def busca_pais(paises, pais):
+    """
+    Paises es un diccionario. Pais es la llave.
+    Codigo con el principio EAFP.
+
+    """
+    error = "El dato ingesado no existe"
+    try:
+        return paises[pais]
+    except KeyError:
+        return error
+
+paises = {
+    'Venezuela': 'Caracas',
+    'Colombia': 'Bogota',
+    'Argentina': 'Cordova',
+    'Canada': 'Ottawa'
+}
+
+if __name__ == '__main__':
+    print(busca_pais(paises, 'Venezuela'))
+    print(busca_pais(paises, 'Colombia'))
+    print(busca_pais(paises, 'Italia'))
