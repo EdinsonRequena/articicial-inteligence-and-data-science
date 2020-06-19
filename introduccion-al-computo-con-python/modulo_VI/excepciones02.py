0bd8219aac56e8cc0bd7ea24682198b54c7d568c
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-19 01:56:00 -0400

    update

--- introduccion-al-computo-con-python/modulo_VI/excepciones02.py
+++ introduccion-al-computo-con-python/modulo_VI/excepciones02.py
@@ -7,3 +7,20 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+def primera_letra(lista_de_palabras):
+    primeras_letras = []
+    
+    for palabra in lista_de_palabras:
+        assert type(palabra) == str, f'{palabra} no es str'
+        assert len(palabra) > 0, 'no se permiten srt vacios'
+        
+        primeras_letras.append(palabra[0])
+            
+    return primeras_letras
+
+if __name__ == '__main__':
+    hola = 'hola'
+    lista_de_palabras = [hola]
+    
+    print(primera_letra(lista_de_palabras))
