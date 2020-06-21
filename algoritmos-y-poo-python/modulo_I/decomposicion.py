ddd913fd5186bb5a02dea8cb8821ff0c101a816f
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-21 16:46:13 -0400

    update

--- algoritmos-y-poo-python/modulo_I/decomposicion.py
+++ algoritmos-y-poo-python/modulo_I/decomposicion.py
@@ -10,25 +10,40 @@ Alumno: @edinsonrequena.
 
 class Computadora:
 
-    def __init__(self, marca, modelo, color, tipo, sistema_operativo, estado=False):
+    def __init__(self, marca, tipo ,modelo, color):
         self.marca = marca
         self.modelo = modelo
         self.color = color
         self.tipo = tipo
-        self.sistema_operativo = sistema_operativo
-        self.estado = estado
         self._temperatura = 0
         self._config_teclado = 'Americano'
 
+
+    def encender(self, estado=False):
+        if estado:
+            self._temperatura += 20
+            print(f'La pc esta encencida, su temperatura es {self._temperatura}!')
+        else:
+            print(f'La pc esta apagada, su temperatura es {self._temperatura}')
+
+    def sistema(self, sistema_operativo):
+        if sistema_operativo == 'linux':
+            print('Tu si que eres un verdadero cientifico computacional!')
+        elif sistema_operativo == 'mac os':
+            print('Eres un ingeniero de software que invierte en sus equipos para optimizar su tiempo')
+        elif sistema_operativo == 'windows':
+            print('Meeeeh esa excusa de los juegos ya no sirve, hay muchisimos juegos ya para Linux. Estudia e invierte en ti!')
+        else:
+            print('Eres una persona rara!')
+
 class Procesador:
 
-    def __init__(self, fabricante, modelo, frecuencia, nucleos, hilos, cache, funciona=True):
+    def __init__(self, fabricante, modelo, frecuencia, nucleos, hilos, funciona=True):
         self.fabricante = fabricante
         self.modelo = modelo
         self.frecuencia = frecuencia
         self.nucleos = nucleos
         self.hilos = hilos
-        self.cache = cache
         self.funciona = funciona
 
 class MemoriaRam():
@@ -37,3 +52,10 @@ class MemoriaRam():
         self.fabricante = fabricante
         self.capacidad = capacidad
         self.funciona = funciona
+
+if __name__ == '__main__':
+    pc1 = Computadora('HP', 'Laptop','Notebook pro 15', 'Negra')
+    print(pc1.encender(True))
+    print(pc1.sistema('linux'))
+
+    pc1_procesador = Procesador('AMD', 'Ryzen 3 1200', '3.7', '4', '4', True)
