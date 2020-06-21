590867fabcdd2c4ba3f3c5d774eda3b05245a5e9
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-20 23:15:31 -0400

    update

--- algoritmos-y-poo-python/modulo_I/poo01.py
+++ algoritmos-y-poo-python/modulo_I/poo01.py
@@ -8,3 +8,23 @@ Alumno: @edinsonrequena.
 
 """
 
+class Coordenada:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distancia(self, otra_coordendada):
+        x_diff = (self.x - otra_coordendada.x)**2
+        y_diff = (self.y - otra_coordendada.y)**2
+
+        return (x_diff + y_diff)**0.5
+
+
+if __name__ == '__main__':
+    coord_1 = Coordenada(34, 100)
+    coord_2 = Coordenada(4, 8)
+
+    print(coord_1.distancia(coord_2))
+    print(isinstance(True, Coordenada))
+    
