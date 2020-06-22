afca5a3d50aa5d058fe6d385a0554ccfd1d9e4cc
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-22 18:47:51 -0400

    update herencia.py

--- algoritmos-y-poo-python/modulo_I/herencia.py
+++ algoritmos-y-poo-python/modulo_I/herencia.py
@@ -7,3 +7,38 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+class PesimoPresidente:
+
+    def __init__(self, nombre, pais, habilidad):
+        self.nombre = nombre
+        self.pais = pais
+        self.habilidad = habilidad # Tambien se puede programar con sarcasmo
+
+
+    def saludo(self):
+        print(f'Hola mi nombre es {self.nombre}, para la poca fortuna de los habitantes de {self.pais} naci alli y mi habilidad es {self.habilidad}')
+
+
+class Chavez(PesimoPresidente):
+
+    def __init__(self, nombre, pais, habilidad):
+        super().__init__(nombre, pais, habilidad)
+
+
+class Trump(PesimoPresidente):
+
+    def __init__(self, nombre, pais, habilidad):
+        super().__init__(nombre, pais, habilidad)
+
+def main():
+    chavez = Chavez('Hugo chavez', 'Venezuela', 'destruir economias')
+    print(chavez.saludo())
+
+    print('\n')
+
+    trump = Trump('Donald Trump', 'Estados Unidos', 'hacer america una basura otra vez!')
+    print(trump.saludo())
+
+if __name__ == '__main__':
+    main()
