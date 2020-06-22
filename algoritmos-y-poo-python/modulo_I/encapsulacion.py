37dd5ec00b8afd5127ec5e29088f9a09e73b4546
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-21 23:36:59 -0400

    update

--- algoritmos-y-poo-python/modulo_I/encapsulacion.py
+++ algoritmos-y-poo-python/modulo_I/encapsulacion.py
@@ -7,3 +7,25 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+class CasillaVotacion:
+
+    def __init__(self, indentificador, pais):
+        self._indentificador = indentificador
+        self._pais = pais
+        self._region = None
+
+    @property
+    def region(self):
+        return self._region
+    
+    @region.setter
+    def set_region(self, region):
+        if region in self._pais:
+            self._region = region
+        else:
+            raise ValueError(f'la region {region} no es validad en {self._pais}')
+
+casilla = CasillaVotacion(394, ['Caracas', 'Valencia', 'Zulia'])
+print(casilla.region)
+print(casilla._pais)
