54ee087abfdf2b4a5c60ab3f3fdfa68e82698dc4
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-24 11:53:36 -0400

    update polimorfimos.py

--- algoritmos-y-poo-python/modulo_I/polimorfismo.py
+++ algoritmos-y-poo-python/modulo_I/polimorfismo.py
@@ -7,3 +7,44 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+class Cientifica:
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+    
+
+    def habilidad(self):
+        print('Soy muy inteligente')
+    
+
+class Fisico(Cientifica):
+
+    def __init__(self, nombre):
+        super().__init__(nombre)
+    
+
+    def habilidad(self):
+        return super().habilidad()
+
+class Matematico(Cientifica):
+
+    def __init__(self, nombre):
+        super().__init__(nombre)
+    
+    def habilidad(self):
+        print('Todas las ciencias derivan de las matematicas, JA!')
+
+
+def main():
+    cientifico = Cientifica('Andrea')
+    cientifico.habilidad()
+
+    fisico = Fisico('Edinson')
+    fisico.habilidad()
+
+    matematico = Matematico('Oriana')
+    matematico.habilidad()
+
+if __name__ == '__main__':
+    main()
