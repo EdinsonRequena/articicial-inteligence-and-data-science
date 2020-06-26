0b4171f3a29077a39c7ea3ee1724682b8ed580c6
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-26 13:24:09 -0400

    update recursividad-multiple.py

--- algoritmos-y-poo-python/modulo_II/recursividad-multiple.py
+++ algoritmos-y-poo-python/modulo_II/recursividad-multiple.py
@@ -1,9 +1,26 @@
 """
 
-Tema: Complejidad Algoritmica. Notacion asintotica - Ley de la multiplicacion.
+Tema: Complejidad Algoritmica. Notacion asintotica - Recursividad multiple
 Curso: Pensamiento Computacional, 2da entrega.
 Plataforma: Platzi.
 Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+def fibonacci(n):
+    ''' 
+    Recursividad multiple
+    O(2^n)
+    '''
+    
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    return fibonacci(n-1) + fibonacci(n-2)
+
+if __name__ == '__main__':
+    n = 100
+    print(fibonacci(n))
