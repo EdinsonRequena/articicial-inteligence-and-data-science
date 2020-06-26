ae20344fe2b1c65fb4e43e10ec73e8b2b60cd8e4
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-26 01:52:33 -0400

    update conteo-abstracto.py

--- algoritmos-y-poo-python/modulo_II/conteo-abstracto.py
+++ algoritmos-y-poo-python/modulo_II/conteo-abstracto.py
@@ -7,3 +7,25 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+def f(x):
+    respuesta = 0
+
+    for i in range(1000):
+        #print(i)
+        respuesta += 1
+
+    for i in range(x):
+        respuesta += x
+    
+    for i in range(x):
+        for j in range(x):
+            print(j)
+            respuesta += 1
+            respuesta += 1
+
+    return respuesta
+
+if __name__ == '__main__':
+    x = 1000
+    f(x)
