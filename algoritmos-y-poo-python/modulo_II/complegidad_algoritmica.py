fab0e2f59890fdd10226316353d8b7abe7280b91
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-25 21:04:18 -0400

    update complejidad_algoritmica.py

--- algoritmos-y-poo-python/modulo_II/complegidad_algoritmica.py
+++ algoritmos-y-poo-python/modulo_II/complegidad_algoritmica.py
@@ -8,3 +8,34 @@ Alumno: @edinsonrequena.
 
 """
 
+import time
+
+def factorial(n):
+    respuesta = 1
+
+    while n > 1:
+        respuesta *= n
+        n -= 1
+
+    return respuesta
+
+
+def factorial_r(n):
+    if n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+
+if __name__ == '__main__':
+    n = 200000
+
+    comienzo = time.time()
+    factorial(n)
+    final = time.time()
+    print(final - comienzo)
+
+    comienzo = time.time()
+    factorial_r(n)
+    final = time.time()
+    print(final - comienzo)
