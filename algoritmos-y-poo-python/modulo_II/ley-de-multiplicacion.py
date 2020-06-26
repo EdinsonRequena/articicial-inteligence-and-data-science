8f35e5ac883df156c6194e406c6e8b6668fe256a
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-26 12:51:34 -0400

    update ley de la multiplicacion

---
+++ algoritmos-y-poo-python/modulo_II/ley-de-multiplicacion.py
@@ -0,0 +1,10 @@
+"""
+
+Tema: Complejidad Algoritmica. Notacion asintotica - Ley de la multiplicacion.
+Curso: Pensamiento Computacional, 2da entrega.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+
+"""
+
