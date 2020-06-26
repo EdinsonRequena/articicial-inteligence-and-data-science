4d01bd3d85473d094ef223fdef6439e87fd90449
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-26 02:25:17 -0400

    update

---
+++ algoritmos-y-poo-python/modulo_II/ley-de-suma2.py
@@ -0,0 +1,9 @@
+"""
+
+Tema: Complejidad Algoritmica. Notacion asintotica - Ley de suma # 2
+Curso: Pensamiento Computacional, 2da entrega.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+
+"""
