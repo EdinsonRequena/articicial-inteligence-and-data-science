0522c6acf225e5d45dd62fd7e4a009b9789849a0
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-26 02:31:12 -0400

    update

--- algoritmos-y-poo-python/modulo_II/ley-de-suma2.py
+++ algoritmos-y-poo-python/modulo_II/ley-de-suma2.py
@@ -1,9 +1,23 @@
 """
 
-Tema: Complejidad Algoritmica. Notacion asintotica - Ley de suma # 2
+Tema: Complejidad Algoritmica. Notacion asintotica - Ley de suma #2
 Curso: Pensamiento Computacional, 2da entrega.
 Plataforma: Platzi.
 Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 
 """
+
+def f(n):
+    ''' 
+    Ley de suma #2
+    O(n) + O(n * n) = O(n + n^2) = O(n^2)
+    '''
+    for i in range(n):
+        print(i)
+    
+    for i in range(n*n):
+        print(i)
+
+n = 5
+f(n)
