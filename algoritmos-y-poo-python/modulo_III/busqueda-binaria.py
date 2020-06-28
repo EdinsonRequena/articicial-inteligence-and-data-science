bf89fbba54c7690fe4f14b7cfc47f3c68e9fceb6
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-28 19:57:05 -0400

    update

--- algoritmos-y-poo-python/modulo_III/busqueda-binaria.py
+++ algoritmos-y-poo-python/modulo_III/busqueda-binaria.py
@@ -5,3 +5,23 @@ Plataforma: Platzi.
 Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 """
+
+
+def main():
+    tamano_lista = int(input('Tamano paraobjeto la lista: '))
+    objetivo = int(input('Objetivo a encontrar: '))
+
+    lista = [random.randint(0, 100) for i in range(tamano_lista)]
+
+    busqueda_lineal(lista, objetivo)
+    print(lista)
+
+    if objetivo in lista:
+        print(f'El elemento {objetivo} esta en la lista')
+    else:
+        print(f'El elemento {objetivo} no esta en la lista')
+    
+    #print(f'el elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+
+if __name__ == '__main__':
+    main()
