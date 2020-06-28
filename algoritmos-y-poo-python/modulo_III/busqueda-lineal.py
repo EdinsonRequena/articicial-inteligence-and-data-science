ae54c70c9df28fd2f624fc7637a5f06d1834d8d6
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-28 01:06:55 -0400

    update busqueda-lineal.py

--- algoritmos-y-poo-python/modulo_III/busqueda-lineal.py
+++ algoritmos-y-poo-python/modulo_III/busqueda-lineal.py
@@ -1,6 +1,6 @@
 """
 
-Tema: Algoritmos de busqueda y ordenamiento
+Tema: Algoritmos de busqueda y ordenamiento - Busqueda Lineal.
 Curso: Pensamiento Computacional, 2da entrega.
 Plataforma: Platzi.
 Profesor: David Aroesti.
@@ -8,3 +8,45 @@ Alumno: @edinsonrequena.
 
 """
 
+import random
+
+def busqueda_lineal(lista, objetivo):
+    ''' 
+    O(n)
+
+    int lista = []
+    int objetivo
+    match = False
+
+    if objetivo in lista:
+        match = True
+    return match
+    '''
+    match = False
+
+    for elemento in lista: # 0(n)
+        if elemento == objetivo:
+            match = True
+            break
+    
+    return match
+
+
+def main():
+    tamano_lista = int(input('Tamano paraobjeto la lista: '))
+    objetivo = int(input('Objetivo a encontrar: '))
+
+    lista = [random.randint(0, 100) for i in range(tamano_lista)]
+
+    busqueda_lineal(lista, objetivo)
+    print(lista)
+
+    if objetivo in lista:
+        print(f'El elemento {objetivo} esta en la lista')
+    else:
+        print(f'El elemento {objetivo} no esta en la lista')
+    
+    #print(f'el elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+
+if __name__ == '__main__':
+    main()
