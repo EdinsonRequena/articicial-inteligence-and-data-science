13d88d8cdfc7de218df2e10e46014d51e9ca7748
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-28 19:53:24 -0400

    update

---
+++ algoritmos-y-poo-python/modulo_III/busqueda-binaria.py
@@ -0,0 +1,7 @@
+"""
+Tema: Algoritmos de busqueda y ordenamiento - Busqueda Binaria.
+Curso: Pensamiento Computacional, 2da entrega.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+"""
