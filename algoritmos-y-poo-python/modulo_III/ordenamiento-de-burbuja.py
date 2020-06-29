9d6099a11498a2bfad73af022a600424d4d44b42
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-29 14:19:48 -0400

    update

--- algoritmos-y-poo-python/modulo_III/ordenamiento-de-burbuja.py
+++ algoritmos-y-poo-python/modulo_III/ordenamiento-de-burbuja.py
@@ -6,3 +6,23 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 """
 
+import random
+
+def main():
+    tamano_lista = int(input('Tamano paraobjeto la lista: '))
+    objetivo = int(input('Objetivo a encontrar: '))
+
+    lista = [random.randint(0, 100) for i in range(tamano_lista)]
+
+    busqueda_binaria(lista, 0, len(lista), objetivo)
+    print(lista)
+
+    if objetivo in lista:
+        print(f'El elemento {objetivo} esta en la lista')
+    else:
+        print(f'El elemento {objetivo} no esta en la lista')
+    
+    #print(f'el elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+
+if __name__ == '__main__':
+    main()
