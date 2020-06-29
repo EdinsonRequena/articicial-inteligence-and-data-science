18c226c66ef988ae2003e428530562e5b1742cd0
EdinsonRequena <edinsonrequena@protonmail.com>
2020-06-28 20:46:56 -0400

    update

--- algoritmos-y-poo-python/modulo_III/busqueda-binaria.py
+++ algoritmos-y-poo-python/modulo_III/busqueda-binaria.py
@@ -5,15 +5,31 @@ Plataforma: Platzi.
 Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 """
+import random
+
+def busqueda_binaria(lista, comienzo, final, objetivo):
+    print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final - 1]}')
+
+    if comienzo > final:
+        return False
+    
+    mitad = (comienzo + final) // 2
+
+    if lista[mitad] == objetivo:
+        return True
+    elif lista[mitad] < objetivo:
+        return busqueda_binaria(lista, mitad + 1, final, objetivo)
+    elif lista[mitad] > objetivo:
+        return busqueda_binaria(lista, comienzo, mitad - 1, objetivo)
 
 
 def main():
     tamano_lista = int(input('Tamano paraobjeto la lista: '))
     objetivo = int(input('Objetivo a encontrar: '))
 
-    lista = [random.randint(0, 100) for i in range(tamano_lista)]
+    lista = sorted([random.randint(0, 100) for i in range(tamano_lista)])
 
-    busqueda_lineal(lista, objetivo)
+    busqueda_binaria(lista, 0, len(lista), objetivo)
     print(lista)
 
     if objetivo in lista:
