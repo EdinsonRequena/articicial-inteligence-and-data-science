722572ed2df3119e28421aec8fa498c27da2b1cc
EdinsonRequena <edinsonrequena@protonmail.com>
2020-07-01 02:36:09 -0400

    update

--- algoritmos-y-poo-python/modulo_III/ordenamiento-de-burbuja.py
+++ algoritmos-y-poo-python/modulo_III/ordenamiento-de-burbuja.py
@@ -8,21 +8,28 @@ Alumno: @edinsonrequena.
 
 import random
 
+def ordenamiento_burbuja(lista):
+    ''' 
+    O(n) * O(n) = O(n * n) = O(n^2)
+
+    int n = []
+    int n = len(n)
+    return n
+    '''
+    n = len(lista)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            pass
+
+
 def main():
     tamano_lista = int(input('Tamano paraobjeto la lista: '))
-    objetivo = int(input('Objetivo a encontrar: '))
 
     lista = [random.randint(0, 100) for i in range(tamano_lista)]
 
-    busqueda_binaria(lista, 0, len(lista), objetivo)
-    print(lista)
-
-    if objetivo in lista:
-        print(f'El elemento {objetivo} esta en la lista')
-    else:
-        print(f'El elemento {objetivo} no esta en la lista')
-    
-    #print(f'el elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+    lista_ordenada = ordenamiento_burbuja(lista)
+    print(lista_ordenada)
 
 if __name__ == '__main__':
     main()
