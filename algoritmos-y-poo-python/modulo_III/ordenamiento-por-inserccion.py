820c7d448ece3e401a67505e17621b059c6fb2d6
EdinsonRequena <edinsonrequena@protonmail.com>
2020-07-01 23:48:26 -0400

    update ordenamiento por inserccion

---
+++ algoritmos-y-poo-python/modulo_III/ordenamiento-por-inserccion.py
@@ -0,0 +1,7 @@
+"""
+Tema: Algoritmos de busqueda y ordenamiento - Ordenamiento por Inserccion.
+Curso: Pensamiento Computacional, 2da entrega.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+"""
