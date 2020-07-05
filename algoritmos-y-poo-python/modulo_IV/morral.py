53e44e5e4a06d211e13f121fe741c2b65bbf6316
EdinsonRequena <edinsonrequena@protonmail.com>
2020-07-05 18:43:00 -0400

    update morral.py

---
+++ algoritmos-y-poo-python/modulo_IV/morral.py
@@ -0,0 +1,8 @@
+"""
+Tema: Algoritmos de optimizacion - 1-0 Problema del Morral.
+Curso: Pensamiento Computacional, 2da entrega.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+"""
+
