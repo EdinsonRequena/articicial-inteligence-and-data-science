1ec66a404f2e429ccbd3045ef9117cfc24a49f4d
EdinsonRequena <edinsonrequena@protonmail.com>
2020-07-05 23:42:58 -0400

    update

--- programacion-dinamica-y-estocastica/modulo_I/programacion-dinamica.py
+++ programacion-dinamica-y-estocastica/modulo_I/programacion-dinamica.py
@@ -8,14 +8,19 @@ Alumno: @edinsonrequena.
 
 import sys
 
+# Fibonacci poco eficiente
 def fibonacci_recursivo(n):
+
+    # Caso Base
     if n == 0 or n == 1:
         return 1
 
     return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
 
-
+# Fibonacci mas eficiente
 def fibonacci_dinamico(n, memo = {}):
+
+    # Caso Base
     if n == 0 or n == 1:
         return 1
 
@@ -27,8 +32,12 @@ def fibonacci_dinamico(n, memo = {}):
 
         return resultado
 
-if __name__ == '__main__':
+
+def main():
     sys.setrecursionlimit(10002)
     n = int(input('Escoge un numero: '))
     resultado = fibonacci_dinamico(n)
     print(resultado)
+
+if __name__ == '__main__':
+    main()
