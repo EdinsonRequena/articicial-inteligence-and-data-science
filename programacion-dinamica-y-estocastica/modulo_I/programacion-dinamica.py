89f9abbb4f5070e6f326f52c86d7d7c8194ed4cb
EdinsonRequena <edinsonrequena@protonmail.com>
2020-07-05 23:21:41 -0400

    update

--- programacion-dinamica-y-estocastica/modulo_I/programacion-dinamica.py
+++ programacion-dinamica-y-estocastica/modulo_I/programacion-dinamica.py
@@ -6,3 +6,29 @@ Profesor: David Aroesti.
 Alumno: @edinsonrequena.
 """
 
+import sys
+
+def fibonacci_recursivo(n):
+    if n == 0 or n == 1:
+        return 1
+
+    return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
+
+
+def fibonacci_dinamico(n, memo = {}):
+    if n == 0 or n == 1:
+        return 1
+
+    try:
+        return memo[n]
+    except KeyError:
+        resultado = fibonacci_dinamico(n - 1, memo) + fibonacci_dinamico(n - 2, memo)
+        memo[n] = resultado
+
+        return resultado
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(10002)
+    n = int(input('Escoge un numero: '))
+    resultado = fibonacci_dinamico(n)
+    print(resultado)
