6c74c5a88d8f4d270b1c455129234d8eaf2ad49a
edinsonrequena <edinsonrequena@protonmail.com>
2021-02-09 13:37:07 -0400

    update

---
+++ algoritmos-y-poo-python/modulo_I/clase01-instancias.py
@@ -0,0 +1,30 @@
+"""
+
+Tema: Programacion Orientada a Objetos.
+Curso: Pensamiento Computacional, 2da entrega.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+
+"""
+
+class Coordenada:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distancia(self, otra_coordendada):
+        x_diff = (self.x - otra_coordendada.x)**2
+        y_diff = (self.y - otra_coordendada.y)**2
+
+        return (x_diff + y_diff)**0.5
+
+
+if __name__ == '__main__':
+    coord_1 = Coordenada(34, 100)
+    coord_2 = Coordenada(4, 8)
+
+    print(coord_1.distancia(coord_2))
+    print(isinstance(True, Coordenada))
+
