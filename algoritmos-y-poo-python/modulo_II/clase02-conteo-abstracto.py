3268a953b97980ba3efccf852b62c685df721688
edinsonrequena <edinsonrequena@protonmail.com>
2021-02-23 12:05:58 -0400

    update

--- algoritmos-y-poo-python/modulo_II/clase02-conteo-abstracto.py
+++ algoritmos-y-poo-python/modulo_II/clase02-conteo-abstracto.py
@@ -9,23 +9,31 @@ Alumno: @edinsonrequena.
 """
 
 def f(x):
+
     respuesta = 0
 
     for i in range(1000):
-        #print(i)
+        print(i)
         respuesta += 1
 
     for i in range(x):
         respuesta += x
-    
+
     for i in range(x):
         for j in range(x):
+            print(i)
             print(j)
             respuesta += 1
             respuesta += 1
 
     return respuesta
 
-if __name__ == '__main__':
+
+def main():
+
     x = 1000
     f(x)
+
+if __name__ == '__main__':
+
+    main()
