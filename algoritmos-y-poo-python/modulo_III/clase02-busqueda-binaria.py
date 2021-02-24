3acbef4cd5194f27e4c132d50202937070627203
edinsonrequena <edinsonrequena@protonmail.com>
2021-02-24 15:17:51 -0400

    update

--- algoritmos-y-poo-python/modulo_III/clase02-busqueda-binaria.py
+++ algoritmos-y-poo-python/modulo_III/clase02-busqueda-binaria.py
@@ -8,11 +8,12 @@ Alumno: @edinsonrequena.
 import random
 
 def busqueda_binaria(lista, comienzo, final, objetivo):
+
     print(f'buscando {objetivo} entre {lista[comienzo]} y {lista[final - 1]}')
 
     if comienzo > final:
         return False
-    
+
     mitad = (comienzo + final) // 2
 
     if lista[mitad] == objetivo:
@@ -24,20 +25,17 @@ def busqueda_binaria(lista, comienzo, final, objetivo):
 
 
 def main():
-    tamano_lista = int(input('Tamano paraobjeto la lista: '))
+
+    tamano_lista = int(input('Tamano de la lista: '))
     objetivo = int(input('Objetivo a encontrar: '))
 
     lista = sorted([random.randint(0, 100) for i in range(tamano_lista)])
 
-    busqueda_binaria(lista, 0, len(lista), objetivo)
+    encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
     print(lista)
 
-    if objetivo in lista:
-        print(f'El elemento {objetivo} esta en la lista')
-    else:
-        print(f'El elemento {objetivo} no esta en la lista')
-    
-    #print(f'el elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
+    print(f'el elemento {objetivo} {"esta" if encontrado else "no esta"} en la lista')
 
 if __name__ == '__main__':
+
     main()
