d3f31bcd68b449680e2e95160b257611d7debca2
edinsonrequena <edinsonrequena@protonmail.com>
2021-02-25 17:39:10 -0400

    update

---
+++ algoritmos-y-poo-python/modulo_III/complementos/eastereggs/easter-egg-bubble-sort.py
@@ -0,0 +1,13 @@
+"""
+1) Abrir Firefox (en principio).
+2) Dirigirse a youtube.com
+3) Dentro de youtube encontrar el elemento html del buscador.
+4) Escribir en el buscador: Bubble-sort with Hungarian ("Csángó") folk dance.
+5) Una vez se haya escrito la palabra encontrar el elemento que haga el query (lupa).
+6) Dar enter al elemento del punto anterior.
+7) Encontrar el elemento donde esta el video del punto 4
+8) Dar enter al elemento del punto anterior para que el video se reproduzca.
+
+FIN  =)
+
+"""
