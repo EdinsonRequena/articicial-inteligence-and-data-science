5e9da760291629e9525f17f145553598e461b66d
edinsonrequena <edinsonrequena@protonmail.com>
2021-02-25 14:00:41 -0400

    update

---
+++ algoritmos-y-poo-python/modulo_III/clase03-ordenamiento-de-burbuja.py
@@ -0,0 +1,40 @@
+"""
+Tema: Algoritmos de busqueda y ordenamiento - Ordenamiento de Burbuja.
+Curso: Pensamiento Computacional, 2da entrega.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+"""
+
+import random
+
+def ordenamiento_burbuja(lista):
+    '''
+    O(n) * O(n - i - 1) = O(n) * O(n) = O(n * n) = O(n^2)
+
+    int n = []
+    int n = len(n)
+    return n
+    '''
+    n = len(lista)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+
+    return lista
+
+
+def main():
+
+    tamano_lista = int(input('Tamano paraobjeto la lista: '))
+
+    lista = [random.randint(0, 100) for i in range(tamano_lista)]
+
+    lista_ordenada = ordenamiento_burbuja(lista)
+    print(lista_ordenada)
+
+if __name__ == '__main__':
+
+    main()
--- algoritmos-y-poo-python/modulo_III/complementos/04-ordenamiento-burbuja-mi-aporte.py
+++ algoritmos-y-poo-python/modulo_III/complementos/04-ordenamiento-burbuja-mi-aporte.py
@@ -9,7 +9,7 @@ Alumno: @edinsonrequena.
 import random
 
 def ordenamiento_burbuja(lista):
-    ''' 
+    '''
     O(n) * O(n - i - 1) = O(n) * O(n) = O(n * n) = O(n^2)
 
     int n = []
@@ -22,12 +22,14 @@ def ordenamiento_burbuja(lista):
         for j in range(0, n - i - 1):
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
-    
+                print(lista)
+
     return lista
 
 
 def main():
-    tamano_lista = int(input('Tamano paraobjeto la lista: '))
+
+    tamano_lista = int(input('Tamano para lista la lista: '))
 
     lista = [random.randint(0, 100) for i in range(tamano_lista)]
 
@@ -35,4 +37,5 @@ def main():
     print(lista_ordenada)
 
 if __name__ == '__main__':
+
     main()
