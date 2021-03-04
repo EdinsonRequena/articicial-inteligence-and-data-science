c4ed9ca775a10150e2160502dd985c282a60d7a7
edinson requena <edinsonrequena@protonmail.com>
2021-03-04 00:16:33 -0400

    update

--- selenium-course/modulo_II/experimentos/repuestos-test.py
+++ selenium-course/modulo_II/experimentos/repuestos-test.py
@@ -1,7 +1,6 @@
 """
 
-Test automatizados para repuestos.
-Verision: 0.2
+Test automatizados para seccion mis datos.
 
 """
 
@@ -22,7 +21,7 @@ class HomePageTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
 
-        cls.driver = webdriver.Firefox(executable_path = '/home/edinson/Descargas/geckodriver')
+        cls.driver = webdriver.Firefox(executable_path = '/home/edinson/Escritorio/geckodriver')
         driver = cls.driver
         driver.get('https://testrepuesto.westus.cloudapp.azure.com/')
         driver.maximize_window()
@@ -53,13 +52,38 @@ class HomePageTests(unittest.TestCase):
 
         datos_button = self.driver.find_element_by_xpath('/html/body/div/div/nav/ul/li[1]/a').click()
 
-    def test_e_email_field(self):
+    def test_e_first_name_field(self):
+
+        name_field = self.driver.find_element_by_id('name')
+        name_field.clear()
+        name_field.send_keys('test')
+
+    def test_f_last_name_field(self):
+
+        last_name_field = self.driver.find_element_by_id('last_name')
+        last_name_field.clear()
+        last_name_field.send_keys('test')
+
+
+    def test_h_email_field(self):
 
         email_field = self.driver.find_element_by_id('email')
         email_field.clear()
         email_field.send_keys('empresa')
 
-    def test_f_save_button(self):
+    def test_i_phone_field(self):
+
+        phone_field = self.driver.find_element_by_id('phone_number')
+        phone_field.clear()
+        phone_field.send_keys('test')
+
+    def test_j_description_field(self):
+
+        descrtiption_field = self.driver.find_element_by_id('properties[about]')
+        descrtiption_field.clear()
+        descrtiption_field.send_keys('test')
+
+    def test_z_save_button(self):
 
         save_button = self.driver.find_element_by_xpath('//*[@id="app"]/div/div/div/div[2]/div/div/form/div[2]/div/button')
         save_button.click()
