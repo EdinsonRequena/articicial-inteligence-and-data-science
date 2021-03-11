de46a11dd5a0ca1f44756630db5c07a8f537577b
edinsonrequena <edinsonrequena@protonmail.com>
2021-03-11 11:09:06 -0400

    update

--- algoritmos-y-poo-python/modulo_III/clase05-ordenamiento-por-mezcla.py
+++ algoritmos-y-poo-python/modulo_III/clase05-ordenamiento-por-mezcla.py
@@ -14,26 +14,26 @@ def ordenamiento_por_mezcla(lista):
 
     # Caso base
     if len(lista) > 1:
-        medio = len(lista) // 2 # Parte la lista a la mitad
+        medio = len(lista) // 2
         izquierda = lista[:medio]
         derecha = lista[medio:]
         print(izquierda, '*' * 5, derecha)
 
-        # Llamada recursiva a cada mitad
-        ordenamiento_por_mezcla(izquierda) # Llamada de [:mitad]
-        ordenamiento_por_mezcla(derecha) # Llamada de [mitad:]
+        # llamada recursiva en cada mitad
+        ordenamiento_por_mezcla(izquierda)
+        ordenamiento_por_mezcla(derecha)
 
         # Iteradores para recorrer las dos sublistas
-        i, j = 0, 0
-
+        i = 0
+        j = 0
         # Iterador para la lista principal
         k = 0
 
-        while i < len(izquierda) and j < len(derecha): # Comparar las dos sublistas
+        while i < len(izquierda) and j < len(derecha):
             if izquierda[i] < derecha[j]:
                 lista[k] = izquierda[i]
                 i += 1
-            elif derecha[j] < izquierda[i]:
+            else:
                 lista[k] = derecha[j]
                 j += 1
 
@@ -42,17 +42,19 @@ def ordenamiento_por_mezcla(lista):
         while i < len(izquierda):
             lista[k] = izquierda[i]
             i += 1
-            k += 1
+            k +=1
 
         while j < len(derecha):
             lista[k] = derecha[j]
             j += 1
             k += 1
+
         print(f'izquierda {izquierda}, derecha {derecha}')
         print(lista)
         print('-' * 50)
 
     return lista
+
 
 def main():
     '''
