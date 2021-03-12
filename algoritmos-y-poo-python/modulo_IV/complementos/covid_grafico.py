ba1c13015b55e2ae977503037c22613f5d296f8a
edinsonrequena <edinsonrequena@protonmail.com>
2021-03-12 14:43:39 -0400

    update

--- algoritmos-y-poo-python/modulo_IV/complementos/covid_grafico.py
+++ algoritmos-y-poo-python/modulo_IV/complementos/covid_grafico.py
@@ -8,3 +8,10 @@ from bokeh.plotting import figure, output_file, show
 if __name__ == '__main__':
 
     URL = "https://corona.lmao.ninja/v2/countries?yesterday&sort"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", URL, headers=headers, data=payload)
+
+    print(response.text)
