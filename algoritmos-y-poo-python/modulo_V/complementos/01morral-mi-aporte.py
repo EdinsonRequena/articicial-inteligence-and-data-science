63e4462f90522c4e56cfc724e6b708300ff91e8e
edinsonrequena <edinsonrequena@protonmail.com>
2021-03-17 09:26:59 -0400

    agregando mi solucion a 1-0 Problema del Morral

--- algoritmos-y-poo-python/modulo_V/complementos/01morral-mi-aporte.py
+++ algoritmos-y-poo-python/modulo_V/complementos/01morral-mi-aporte.py
@@ -3,6 +3,8 @@ Tema: Algoritmos de optimizacion - 1-0 Problema del Morral.
 Alumno: @edinsonrequena.
 """
 
+import random
+
 def morral(tamano_morral, pesos, valores, n):
 
     # Caso base 1
@@ -19,9 +21,15 @@ def morral(tamano_morral, pesos, valores, n):
 
 def main():
 
-    valores = [60, 100, 120]
-    pesos = [10, 20, 30]
-    tamano_morral = 50
+    tamano_morral = int(input('Escoge el peso del morral: '))
+    cantidad = int(input('Escoge la cantidad de elementos y valores: '))
+
+    pesos = [random.randint(0, 100) for i in range(cantidad)]
+    valores = [random.randint(0, 100) for i in range(cantidad)]
+
+    print(f'Los pesos son {pesos}')
+    print(f'Los valores son {valores}')
+
     n = len(valores)
     print(n)
 
