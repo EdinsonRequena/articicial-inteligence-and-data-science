63a94003da50f22712490fd65c5c833b670fccf9
edinson requena <edinsonrequena@protonmail.com>
2021-03-21 22:16:29 -0400

    update

---
+++ introduccion-al-computo-con-python/modulo_II/clase01-aproximacion.py
@@ -0,0 +1,23 @@
+"""
+
+Tema: Algoritmo de aproximacion.
+Curso: Pensamiento computacional.
+Plataforma: Platzi.
+Profesor: David Aroesti.
+Alumno: @edinsonrequena.
+
+"""
+
+objetivo = int(input('Escoge un numero: '))
+epsilon = 0.0001
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    print(abs(respuesta**2 - objetivo), respuesta)
+    respuesta += paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'No se encontro la raiz cuadrada {objetivo}')
+else:
+    print(f'La raiz cudrada de {objetivo} es {respuesta}')
