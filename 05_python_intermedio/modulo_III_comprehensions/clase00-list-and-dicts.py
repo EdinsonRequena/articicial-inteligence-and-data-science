b71119fce79e7669c9916f37d5c28ede0916636e
edinson requena <edinsonrequena@protonmail.com>
2021-04-18 22:15:12 -0400

    Agregando Listas y Diccionarios Anidados - Curso Intermedio de Python

---
+++ 05_python_intermedio/modulo_III_comprehensions/clase00-list-and-dicts.py
@@ -0,0 +1,34 @@
+"""
+
+Tema: Listas y Diccionarios anidados
+Curso: Python intermedio.
+Plataforma: Platzi.
+Profesor: Facundo García Martoni.
+Alumno: @edinsonrequena.
+
+"""
+
+def main():
+    my_list = [1, 'hey!', False, 35.2]
+    my_dict = {'First name': 'Edinson', 'La t_name': 'Requena'}
+
+    super_list = [
+        {'First name': 'Edinson', 'Las name': 'Requena'},
+        {'First name': 'Andrea', 'Last name': 'Vinas'},
+        {'First name': 'Diana', 'Last name': 'Quintero'},
+        {'First name': 'Vicky', 'Last name': 'Aguilar'},
+    ]
+
+    super_dict = {
+        'Natural Numbers': [1, 2, 3, 4, 5],
+        'Integers Numbers': [-2, -1, 0, 1, 2],
+        'Racional NUmbers': [4.23, 20/5, 10/2],
+    }
+
+    for k, v in super_dict.items():
+        print(k, '-', v)
+
+
+if __name__ == '__main__':
+
+    main()
