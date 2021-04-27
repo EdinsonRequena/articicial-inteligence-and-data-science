425ec07e4da85b7790906698e590b86158fc8e2f
edinson requena <edinsonrequena@protonmail.com>
2021-04-26 21:45:35 -0400

    Agregando Contenido extra - Curso Intermedio de Python

---
+++ 05_python_intermedio/modulo_IV_conceptos_avanzados_de_funciones/complementos/pildorasinformaticas/funciones_lambda/practica00.py
@@ -0,0 +1,21 @@
+"""
+
+Tema: Funciones Lambda
+Curso: Python.
+Plataforma: Youtube.
+Profesor: Juan Diaz (Pildoras informaticas).
+Alumno: @edinsonrequena.
+
+"""
+
+#  Calcular el area de un triangulo
+area_triangulo = lambda base, altura: (base*altura) / 2
+
+# Elevar un numero al cubo
+cubo = lambda x: x**3
+
+#  Elevar un numero al cuadrado
+cuadrado = lambda x: x**2
+
+# Reto
+desctacar = lambda string: f'{string}$ !!!'
