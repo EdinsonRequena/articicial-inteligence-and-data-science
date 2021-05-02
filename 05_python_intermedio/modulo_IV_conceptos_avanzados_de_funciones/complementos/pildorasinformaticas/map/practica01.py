a549c31476395da76b7e0890373f57a822ec6c02
edinson requena <edinsonrequena@protonmail.com>
2021-05-02 14:08:27 -0400

    Actualizando Contenido - Curso Intermedio de Python

--- 05_python_intermedio/modulo_IV_conceptos_avanzados_de_funciones/complementos/pildorasinformaticas/map/practica01.py
+++ 05_python_intermedio/modulo_IV_conceptos_avanzados_de_funciones/complementos/pildorasinformaticas/map/practica01.py
@@ -24,6 +24,6 @@ listaEmpleados = [
     Empleado('Tes2', 'Dev', 3000),
 ]
 
-listaEmpleadosComision = [i.salario for i in listaEmpleados if i.salario <= 3000]
+listaEmpleadosComision = [i.salario for i in listaEmpleados if i.salario <= 3000] #  Meto dentro de listaEmpleadosComision solo a los elementos de listaEmpleados cuyo salario sea menor a 3000
 listaMapeada = map(lambda arg: arg * 1.03, listaEmpleadosComision)
 [print(i) for i in listaMapeada]
