940edb497e679d02e3011ec0c54d5d6e8feae96b
edinson requena <edinsonrequena@protonmail.com>
2021-05-03 22:27:28 -0400

    Proyecto de filtrado de datos - Curso Intermedio de Python

---
+++ 05_python_intermedio/modulo_IV_conceptos_avanzados_de_funciones/clase02-filtrando-datos.py
@@ -0,0 +1,99 @@
+"""
+
+Tema: Hig Order Functions.
+Curso: Python intermedio.
+Plataforma: Platzi.
+Profesor: Facundo García Martoni.
+Alumno: @edinsonrequena.
+
+"""
+
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+
+def main():
+
+    all_python_devs = [worker['name'] for worker in DATA if worker['language'] == 'python']
+    all_platzi_workes = [worker['name'] for worker in DATA if worker['organization'] == 'Platzi']
+
+    adults = list(filter(lambda arg: arg['age'] > 18, DATA))
+    adults_mapping = list(map(lambda arg: arg['name'], adults))
+    old_people = list(map(lambda arg: arg | {'old': arg['age'] > 70}, DATA))
+
+    [print(i) for i in old_people]
+
+
+if __name__ == '__main__':
+
+    main()
