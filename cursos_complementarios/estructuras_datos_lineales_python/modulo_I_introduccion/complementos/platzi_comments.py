021b513bb9907aba4c47b8b58be13ccd6390d18b
edinson requena <edinsonrequena@protonmail.com>
2021-07-04 18:48:48 -0400

    Colecciones en Python (Correcciones PEP8) - Curso de Estructuras de datos Lineales en Python

--- cursos_complementarios/estructuras_datos_lineales_python/modulo_I_introduccion/complementos/platzi_comments.py
+++ cursos_complementarios/estructuras_datos_lineales_python/modulo_I_introduccion/complementos/platzi_comments.py
@@ -11,7 +11,7 @@ def func_list() -> list:
 
     list1 = list()
     list2 = [False, any, 'Hola', 5, 3.45]
-    list3 = [n**2 for n in range(1, 100) if n%2 == 0]
+    list3 = [n**2 for n in range(1, 100) if n % 2 == 0]
 
     print(list1)
     print(list2)
