4ba55cafe710a4cd6bba96633d5ba9eef04e0af9
edinson requena <edinsonrequena@protonmail.com>
2021-07-05 00:20:33 -0400

    Creando Array - Curso de Estructuras de datos Lineales en Python

--- cursos_complementarios/estructuras_datos_lineales_python/modulo_II_arrays/clase00_crear_array.py
+++ cursos_complementarios/estructuras_datos_lineales_python/modulo_II_arrays/clase00_crear_array.py
@@ -7,44 +7,56 @@ Alumno: @edinsonrequena.
 """
 
 class Array(object):
-    "Represents an array."
+    """
+    Create a own Array
+    """
 
-    def __init__(self, capacity, fill_value = None):
-        """
-        Args:
-            capacity (int): static size of the array.
-            fill_value (any, optional): value at each position. Defaults to None.
-        """
+    def __init__(self, capacity: int, fill_value = None) -> None:
         self.items = list()
         for i in range(capacity):
             self.items.append(fill_value)
 
-    def __len__(self):
-        """Returns capacity of the array."""
-        return len(self.items)
+    def __len__(self) -> int:
+        """
+        Method to Know the array's lenght
+        """
+        count = 0
+        for i in self.items:
+            count += 1
+        return count
 
-    def __str__(self):
+    def __str__(self) -> str:
         """Returns string representation of the array"""
         return str(self.items)
 
     def __iter__(self):
-        """Supports traversal with a for loop."""
-        return iter(self.items)
+        """
+        Method to iter the array
+        """
+        current = 0
+        while current < len(self.items):
+            yield current
+            current += 1
 
     def __getitem__(self, index):
-        """Subscrit operator for access at index."""
+        """
+        method to get a specific index
+        """
         return self.items[index]
 
     def __setitem__(self, index, new_item):
-        """Subscript operator for replacement at index."""
+        """
+        Method to set item in a specific index
+        """
         self.items[index] = new_item
+        return self.items
 
 arr = Array(5)
 print(len(arr))
 print(arr)
-print(arr.__len__())
 for i in range(len(arr)):
     arr[i] = i + 1
-print(arr.__iter__())
-print(arr.__getitem__(2))
-print(arr.__setitem__(2, 'hola'))
+print(arr)
+print(arr.__len__())
+print(arr.__getitem__(3))
+print(arr.__setitem__(1, 'hola'))
