805d133f9b62cb805856126a87733bfe1b1c4a1b
edinson requena <edinsonrequena@protonmail.com>
2021-07-04 23:58:01 -0400

    Creando Array - Curso de Estructuras de datos Lineales en Python

--- cursos_complementarios/estructuras_datos_lineales_python/modulo_II_arrays/clase00_crear_array.py
+++ cursos_complementarios/estructuras_datos_lineales_python/modulo_II_arrays/clase00_crear_array.py
@@ -7,45 +7,37 @@ Alumno: @edinsonrequena.
 """
 
 class Array(object):
-    """
-    Create a own Array
-    """
+    "Represents an array."
 
-    def __init__(self, capacity, fill_value = None) -> None:
+    def __init__(self, capacity, fill_value = None):
+        """
+        Args:
+            capacity (int): static size of the array.
+            fill_value (any, optional): value at each position. Defaults to None.
+        """
         self.items = list()
         for i in range(capacity):
             self.items.append(fill_value)
 
-    def __len__(self) -> int:
-        """
-        Method to Know the array's lenght
-        """
-        count = 0
-        for i in self.items:
-            count += 1
-        return count
+    def __len__(self):
+        """Returns capacity of the array."""
+        return len(self.items)
+
+    def __str__(self):
+        """Returns string representation of the array"""
+        return str(self.items)
 
     def __iter__(self):
-        """
-        Method to iter the array
-        """
-        current = 0
-        while current < len(self.items):
-            yield current
-            current += 1
+        """Supports traversal with a for loop."""
+        return iter(self.items)
 
-    def __getItem__(self, index):
-        """
-        method to get a specific index
-        """
+    def __getitem__(self, index):
+        """Subscrit operator for access at index."""
         return self.items[index]
 
-    def __setItem__(self, index, new_item):
-        """
-        Method to set item in a specific index
-        """
+    def __setitem__(self, index, new_item):
+        """Subscript operator for replacement at index."""
         self.items[index] = new_item
-        return self.items
 
 arr = Array(5)
 print(len(arr))
@@ -54,5 +46,5 @@ print(arr.__len__())
 for i in range(len(arr)):
     arr[i] = i + 1
 print(arr.__iter__())
-print(arr.__getItem__(2))
-print(arr.__setItem__(2, 'hola'))
+print(arr.__getitem__(2))
+print(arr.__setitem__(2, 'hola'))
