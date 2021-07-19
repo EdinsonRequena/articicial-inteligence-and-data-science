9f563112a69de5e303e3d56b6c279e652ad9646d
edinson requena <edinsonrequena@protonmail.com>
2021-07-18 23:14:34 -0400

    Curso de Estructuras de Datos Python - Nodes

---
+++ cursos_complementarios/estructuras_datos_lineales_python/modulo_III_linked_lists/node.py
@@ -0,0 +1,10 @@
+
+class Node:
+    """Node"""
+
+    def __init__(self, data, next=None) -> None:
+        """
+        Initialize a node
+        """
+        self.data = data
+        self.next = next
