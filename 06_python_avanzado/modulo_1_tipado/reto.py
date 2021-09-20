76a3252e6578ea94d87bff870ae6567b07216409
edinson requena <edinsonrequena@protonmail.com>
2021-09-20 00:27:49 -0400

    Python Profesional Course - PEP8 Corrections

--- 06_python_avanzado/modulo_1_tipado/reto.py
+++ 06_python_avanzado/modulo_1_tipado/reto.py
@@ -8,11 +8,12 @@ import random
 numberListRandom: List[int] = [random.randint(0, 100) for number in range(0, 15)]
 numberList: List[int] = [1, 3, 4, 6, 32, 6, 64, 8, 10, 2, 3, 5, 7, 11, 13, 17]
 
+
 def isPrime(arr: List[int]) -> List:
     """
     Verify if number is prime
     """
-    primeArr: List[int] = [number for number in arr if number %2 != 0]
+    primeArr: List[int] = [number for number in arr if number % 2 != 0]
 
     return primeArr
 
