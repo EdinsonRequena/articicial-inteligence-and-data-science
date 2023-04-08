4de704ddf768a973250c5d43012787f00b040bb2
Edinson Requena <edinsonrequena@protonmail.com>
2023-04-07 23:21:16 -0400

    feat: clase02 in python comprehensions course

---
+++ 010_python_comprhensions_funtions_error_manage/modulo_1_sets/clase-02-crud-sets.py
@@ -0,0 +1,31 @@
+set_countries = {'col', 'mex', 'bol'}
+
+size = len(set_countries)
+print(size)
+
+print('col' in set_countries)
+print('pe' in set_countries)
+
+# add
+set_countries.add('pe')
+print(set_countries)
+set_countries.add('pe')
+print(set_countries)
+
+# update
+set_countries.update({'ar', 'ecua', 'pe'})
+print(set_countries)
+
+# remove
+
+set_countries.remove('col')
+print(set_countries)
+
+set_countries.remove('ar')
+set_countries.discard('arg')
+print(set_countries)
+set_countries.add('arg')
+print(set_countries)
+set_countries.clear()
+print(set_countries)
+print(len(set_countries))
