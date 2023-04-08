e93d437281be419a8b34cb31edcf5b99e4c20d85
Edinson Requena <edinsonrequena@protonmail.com>
2023-04-07 23:41:41 -0400

    feat: clase03 in python comprehensions course

--- 010_python_comprhensions_funtions_error_manage/modulo_1_sets/clase-03-sets-operations.py
+++ 010_python_comprhensions_funtions_error_manage/modulo_1_sets/clase-03-sets-operations.py
@@ -1,3 +1,12 @@
+
+# Operaciones set
+
+# union(set): Realiza la operacion “union” entre dos conjuntos. La unión entre dos conjuntos es sumar los elementos de estos sin repetir elementos. Esta operación tambien se puede realizar con el signo “|”: set_a | set_b.
+# intersection(set): Realiza la operacion “intersection” entre dos conjuntos. La intersección entre dos conjuntos es tomar unicamente los elementos en común de los conjutnos. Esta operación tambien se puede realizar con el signo “&”: set_a & set_b.
+# difference(set): Realiza la operacion “difference” entre dos conjuntos. La diferencia entre dos conjuntos es restar los elementos del segundo conjunto al primero. Esta operación tambien se puede realizar con el signo “-”: set_a - set_b.
+# symmetric_difference(set): Realiza la operacion “symmetric_difference” entre dos conjuntos. La diferencia simetrica entre dos conjutnos consta de restar todos los elementos de ambos exceptuando el elemento en común. Esta operación tambien se puede realizar con el signo “^”: set_a ^ set_b.
+# NOTA: No se pueden realizar operaciones con otras colecciones de datos, solo se puede únicamente entre conjuntos.
+
 set_a = {'col', 'mex', 'bol'}
 set_b = {'pe', 'bol'}
 
